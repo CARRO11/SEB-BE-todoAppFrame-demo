8c373d15a3bb8605dfde69c9f0d5341b3f326fba
CARRO11 <eleying@naver.com>
2023-02-14 17:50:57 +0900

    dev branch

---
+++ src/main/java/com/SEBBEtodoAppFrame/Controller/controller.java
@@ -0,0 +1,12 @@
+package com.SEBBEtodoAppFrame.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+    @GetMapping
+    public String hello(){
+        return "Todo Application !";
+    }
+}
