88f5e6f5d93fb0255f198be42e5d28e175717863
CARRO11 <eleying@naver.com>
2023-02-14 17:14:06 +0900

    Initial commit

---
+++ src/main/java/com/SEBBEtodoAppFrame/demo/SebBeTodoAppFrameApplication.java
@@ -0,0 +1,13 @@
+package com.SEBBEtodoAppFrame.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SebBeTodoAppFrameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SebBeTodoAppFrameApplication.class, args);
+	}
+
+}
