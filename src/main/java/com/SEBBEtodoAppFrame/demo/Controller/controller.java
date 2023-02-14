3f9d1073788484866d5a635deeb3718478f4f37a
CARRO11 <eleying@naver.com>
2023-02-14 18:13:32 +0900

    bb

--- src/main/java/com/SEBBEtodoAppFrame/demo/Controller/controller.java
+++ src/main/java/com/SEBBEtodoAppFrame/demo/Controller/controller.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class controller {
     @GetMapping("/")
+    //branch practice
     public String hello(){
         return "To do Application !";
         //branch
